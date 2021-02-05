7b8de2d7e42658422332e409587199186d205348
Vlatterran <soboleff@mail.ru>
2021-02-05 23:55:20 +0300

    main.py changed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from VkBot import VkBot
-from boto.s3.connection import S3Connection
 import os
 
 
-bot = VkBot(access_token=S3Connection(os.environ['token']), group_id=S3Connection(os.environ['group']))
+bot = VkBot(access_token=os.environ['token'], group_id=os.environ['group'])
 bot.run()
