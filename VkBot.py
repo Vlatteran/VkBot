5b69cf83d4b54d07b0720e15125de78a07a255b8
Vlatterran <soboleff@mail.ru>
2021-02-06 16:48:10 +0300

    some new logging added

--- VkBot.py
+++ VkBot.py
@@ -8,12 +8,15 @@ class VkBot:
     def __init__(self, access_token, group_id):
         self.access_token = access_token
         self.group_id = group_id
-        self.server = LongPollServer(access_token, group_id)
+        self.server = LongPollServer(access_token, group_id, self)
         self.schedule = Schedule()
 
     def run(self):
         for event, context in self.server.listen():
             if event == 'message_new':
+                print(f'[VkBot.run()/on_message_new_event]:'
+                      f'{context.sender.first_name} {context.sender.last_name} '
+                      f'in {context.chat.title}: {context.text}')
                 if context.text[0] == '!':
                     command = context.text[
                               1:len(context.text) if context.text.find(' ') == -1 else context.text.find(' ')]
@@ -26,7 +29,8 @@ class LongPollServer:
     key: str
     ts: str
 
-    def __init__(self, access_token, group_id):
+    def __init__(self, access_token, group_id, vk):
+        self.vk = vk
         self.access_token = access_token
         self.group_id = group_id
         self.get_long_poll_server()
@@ -64,7 +68,7 @@ class LongPollServer:
             events = result['updates']
             for event in events:
                 if event['type'] == 'message_new':
-                    return 'message_new', Message(event['object']['message'])
+                    return 'message_new', Message(event['object']['message'], self.vk)
 
     def listen(self):
         while True:
@@ -77,18 +81,18 @@ class LongPollServer:
 class Message:
     text: str
 
-    def __init__(self, message_dict):
+    def __init__(self, message_dict, vk):
         self.date = message_dict['date']
         self.text = message_dict['text']
-        self.from_id = message_dict['from_id']
-        self.peer_id = message_dict['peer_id']
+        self.sender = User(message_dict['from_id'], vk)
+        self.chat = Chat(message_dict['peer_id'], vk)
         self.conversation_message_id = message_dict['conversation_message_id']
 
     def reply(self, text, vk: VkBot):
         return get(f'https://api.vk.com/method/messages.send?'
-                   f'peer_id={self.peer_id}&'
+                   f'peer_id={self.chat.chat_id}&'
                    f'message={text}&'
-                   f'forward={{"peer_id":{self.peer_id},'
+                   f'forward={{"peer_id":{self.chat.chat_id},'
                    f'"conversation_message_ids":[{self.conversation_message_id}],'
                    f'"is_reply":1}}&'
                    f'group_id={vk.group_id}&'
@@ -98,19 +102,41 @@ class Message:
 
 
 class Chat:
-    def __init__(self):
-        pass
+    def __init__(self, chat_id, vk: VkBot):
+        result = get(f'https://api.vk.com/method/messages.getConversationsById?'
+                     f'peer_ids={chat_id}&'
+                     f'access_token={vk.access_token}&'
+                     f'v=5.126').json()
+        result = result['response']['items'][0]
+        self.chat_id = chat_id
+        self.title = result['chat_settings']['title']
+        self.admins = [User(admin_id, vk) for admin_id in result['chat_settings']['admin_ids'] if admin_id > 0]
+        self.member_count = result['chat_settings']['members_count']
 
-    def send(self):
-        pass
+    def send(self, text, vk):
+        return get(f'https://api.vk.com/method/messages.send?'
+                   f'peer_id={self.chat_id + 2000000000}&'
+                   f'message={text}&'
+                   f'group_id={vk.group_id}&'
+                   f'access_token={vk.access_token}&'
+                   f'random_id={random.randint(1, 2147123123)}&'
+                   f'v=5.126').json()
+
+
+class User:
+    def __init__(self, user_id, vk: VkBot):
+        url = f'https://api.vk.com/method/users.get?' \
+              f'user_ids={user_id}&' \
+              f'access_token={vk.access_token}&' \
+              f'v=5.126'
+        result = get(url).json()['response'][0]
+        self.id = user_id
+        self.first_name = result['first_name']
+        self.last_name = result['last_name']
 
 
 if __name__ == '__main__':
     from config import token, group_id as group
 
     bot = VkBot(access_token=token, group_id=group)
-    while True:
-        try:
-            bot.run()
-        except:
-            pass
+    bot.run()
--- main.py
+++
@@ -1,7 +0,0 @@
-from VkBot import VkBot
-import os
-
-
-print('some shit')
-bot = VkBot(access_token=os.environ['token'], group_id=os.environ['group'])
-bot.run()
