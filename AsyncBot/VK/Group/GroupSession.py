d6d9a5686fe54fbe06b86d803933f7eca8c12f30
Vlatterran <soboleff@mail.ru>
2021-04-13 20:14:12 +0300

    Moved from VkBot to individual file

---
+++ AsyncBot/VK/Group/GroupSession.py
@@ -0,0 +1,7 @@
+from AsyncBot import VK
+
+
+class GroupSession(VK.Session):
+    def __init__(self, access_token: str, group_id: int, api_version: int = 5.126):
+        super().__init__(access_token, api_version=api_version)
+        self.session_params |= {'group_id': group_id}
