a6fc4d8ac82ff6d8076db345e848542c59df9bec
Vlatterran <soboleff@mail.ru>
2021-04-21 11:19:29 +0300

    new call of event_handler

--- AsyncBot/VK/Group/VkBot.py
+++ AsyncBot/VK/Group/VkBot.py
@@ -1,15 +1,14 @@
 import asyncio
 import time
-from typing import *
 
 import AsyncBot.VK.Message
 from AsyncBot import Logger
+from AsyncBot.VK.Group.EventHandler import EventHandler
 from AsyncBot.VK.Group.GroupSession import GroupSession
 from AsyncBot.VK.Group.LongPollServer import LongPollServer
-from AsyncBot.VK.Group.Events import Events
 
 
-class VkBot:
+class VkBot(EventHandler):
     def __init__(self, access_token: str, group_id, bot_admin, logger=None, log_file='', log_to_file=False,
                  log_to_console=True):
         if logger is None:
@@ -21,10 +20,6 @@ class VkBot:
         self.server: LongPollServer = LongPollServer(self.vk_session, logger=logger)
         self.commands: dict = {}
         self.is_running = False
-        self.events: Dict[str: Coroutine] = {
-            Events.MESSAGE_NEW: self.on_message_new,
-            Events.MESSAGE_EDIT: self.on_message_edit
-        }
 
     def start(self):
         asyncio.run(self.run())
@@ -34,16 +29,18 @@ class VkBot:
         try:
             async for event, context in self.server.listen():
                 try:
-                    await self.events[event](**context)
-                except KeyError:
-                    pass
+                    await event(context, self)
+                except KeyError as e:
+                    print(e)
+                except Exception as e:
+                    print(e)
                 if not self.is_running:
                     asyncio.get_event_loop().close()
                     break
         except Exception as e:
             await self.logger.log(f'Crushed with:\n{e}', method_name='VkBot.run()')
 
-    async def on_message_new(self, message: AsyncBot.VK.Message.Message):
+    async def on_message_new(self, message: AsyncBot.VK.Message.Message, client_info: dict):
         asyncio.create_task(
             self.logger.log(
                 text=f'Message from {message.sender.first_name} {message.sender.last_name} ({message.sender.id}) '
